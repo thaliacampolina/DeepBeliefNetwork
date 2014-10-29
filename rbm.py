b8884be059814a322b05bf3b0befd7cfbae7a76f
Thalia Campolina <thaliacampolina@hotmail.com>
2014-10-29 14:00:39 -0200

    add variables to rbm.py

--- rbm.py
+++ rbm.py
@@ -9,16 +9,22 @@ class RBM:
     def __init__(self, data, numhid): #new RBM => restart=1
         self.epoch = 1 #epoch counter
         self.numcases, self.numdims, self.numbatches = data.shape
-        self.vishid = 0.1*numpy.random.randn(self.numdims, numhid)
+        self.vishid    = 0.1*numpy.random.randn(self.numdims, numhid)
         self.hidbiases = numpy.zeros((1, numhid))
         self.visbiases = numpy.zeros((1, self.numdims))
 
+        self.poshidprobs = numpy.zeros((self.numcases, numhid));
+        self.neghidprobs = numpy.zeros((self.numcases, numhid));
+        self.posprods    = numpy.zeros((self.numdims, numhid));
+        self.negprods    = numpy.zeros((self.numdims, numhid));
+        self.vishidinc   = numpy.zeros((self.numdims, numhid));
+        self.hidbiasinc  = numpy.zeros((1, numhid));
+        self.visbiasinc  = numpy.zeros((1, self.numdims));
+        self.batchposhidprobs = numpy.zeros((self.numcases, numhid, self.numbatches));
+
+
     def __repr__(self):
         return  "Matrix: %s \n Data: %s %s %s"%(self.vishid, self.numcases, self.numdims, self.numbatches)
-
-
-
-
 
 
 
