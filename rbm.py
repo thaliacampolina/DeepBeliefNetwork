7f138636054b1db6ac5412ef151f1eee6f957888
Thalia Campolina <thaliacampolina@hotmail.com>
2014-10-29 13:35:12 -0200

    add rbm.py

---
+++ rbm.py
@@ -0,0 +1,29 @@
+import numpy
+
+#Data is divided into lines(numcases), colums(numdims)
+#Batches = number of examples
+#Numhid = number of hidden labels
+#Vishid = weight from visible to hidden layer
+
+class RBM:
+    def __init__(self, data, numhid): #new RBM => restart=1
+        self.epoch = 1 #epoch counter
+        self.numcases, self.numdims, self.numbatches = data.shape
+        self.vishid = 0.1*numpy.random.randn(self.numdims, numhid)
+        self.hidbiases = numpy.zeros((1, numhid))
+        self.visbiases = numpy.zeros((1, self.numdims))
+
+    def __repr__(self):
+        return  "Matrix: %s \n Data: %s %s %s"%(self.vishid, self.numcases, self.numdims, self.numbatches)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    data = numpy.ones((2,3,7));
+    numhid = 3
+    rbm = RBM(data,numhid)
+    print rbm
